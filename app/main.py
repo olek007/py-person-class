60618e597308cf1619dc812b66f5fe6353263424
Aleksander Rokita <kelor2@o2.pl>
2025-03-22 12:50:26 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,38 @@
 class Person:
-    # write your code here
-    pass
+
+    people: dict = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({f"{name}": self})
+
+    def add_wife(self, wife_name: str = None) -> None:
+        wife = self.people.get(wife_name)
+        if wife:
+            self.wife = wife
+            self.wife.husband = self
+
+    def add_husband(self, husband_name: str = None) -> None:
+        husband = self.people.get(husband_name)
+        if husband:
+            self.husband = husband
+            self.husband.wife = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+
+    # Create person instances
+    for person in people:
+        result.append(Person(person["name"], person["age"]))
+
+    # Establish relationships
+    for person in people:
+        person_instance = Person.people[person["name"]]
+        if person.get("wife"):
+            person_instance.add_wife(person["wife"])
+        if person.get("husband"):
+            person_instance.add_husband(person["husband"])
+
+    return result
